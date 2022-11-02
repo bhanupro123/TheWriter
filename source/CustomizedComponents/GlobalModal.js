7359502439788c959e245d685a87f4769789d41f
santhosh-annam <santhosh.annam@devrabbit.com>
2022-11-02 18:11:33 +0530

    final changes

--- source/CustomizedComponents/GlobalModal.js
+++ source/CustomizedComponents/GlobalModal.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useImperativeHandle, useRef, useState,forwardRef } from 'react';
 import { TouchableOpacity,View,Text, Button, ScrollView, Dimensions ,Modal} from 'react-native'; 
-let array=Array.from(Array(50).keys())
+let array=Array.from(Array(5).keys())
 const GlobalModal = forwardRef(({textValue="Text",color="red",...props},ref) => {
      const [isVisible,setIsVissible]=useState(false)
      const [margin,setMargin]=useState(0)
      const [alignTop,setAlignTop]=useState(false)
      const [alignBottom,setAlignBottom]=useState(false)
+     const[maxHeight,setMaxHeight]=useState(null)
      function generateRandomColor(){
       let maxVal = 0xFFFFFF; // 16777215.
       let randomNumber = Math. random() * maxVal;
@@ -17,7 +18,7 @@ const GlobalModal = forwardRef(({textValue="Text",color="red",...props},ref) => 
       }  
      useImperativeHandle(ref, () => ({
       refresh(width, height, pageX, pageY) {
-        console.log( height,pageY) 
+        console.log(width, height, pageX, pageY) 
           if(1208/2>=(pageY))
           {
           console.log("up")
@@ -25,13 +26,14 @@ const GlobalModal = forwardRef(({textValue="Text",color="red",...props},ref) => 
           setAlignBottom(false)
           setMargin(height+pageY)
           setIsVissible(true)
+          setMaxHeight(1028-height-pageY)
           }
-          else{
-            console.log("down",1208-pageY,1208,pageY,height)
+          else{ 
             setAlignTop(false)
             setAlignBottom(true)
             setMargin(1208-pageY?1208-pageY:0)
           setIsVissible(true)
+          setMaxHeight(1028)
           }
       },
 
@@ -41,7 +43,7 @@ const GlobalModal = forwardRef(({textValue="Text",color="red",...props},ref) => 
     }) 
     return ( 
             <Modal transparent  onLayout={(e)=>{
-              console.log("::::",e.nativeEvent.layout)
+           
             }}  style={{margin:0,elevation:5}} onBackdropPress={()=>{
                setIsVissible(false) 
             }}  onBackButtonPress={()=>{
@@ -50,12 +52,12 @@ const GlobalModal = forwardRef(({textValue="Text",color="red",...props},ref) => 
            
             
            <View onLayout={(e)=>{
-            console.log(e.nativeEvent.layout)
-           }}  style={{backgroundColor:'red' ,   width:'100%',bottom:alignBottom?margin:0,top:alignTop?margin:0, position:'absolute'}}>
+               console.log("::::",e.nativeEvent.layout)
+           }}  style={{backgroundColor:'red' ,width:'100%',bottom:alignBottom?margin:'auto',top:alignTop?margin:'auto', position:'absolute',maxHeight:1208-margin }}>
             <Button onPress={()=>{
                 setIsVissible(false)
                }} title={"close"}></Button>
-            <ScrollView style={{width:'100%',backgroundColor:'green'}}>
+            <ScrollView style={{width:'100%'}}>
             {array.map((item,index,key)=>{
                 return  <TouchableOpacity index={index} key={key+index} style={{height:80,backgroundColor:generateRandomColor(),margin:5,alignItems:'center',justifyContent:'center',borderRadius:20,width:'50%',alignSelf:'center'}}>
                     <Text style={{color:'white'}}>{item}</Text>
